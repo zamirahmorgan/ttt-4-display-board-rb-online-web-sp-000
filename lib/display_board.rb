21ae9dbcac949bfb032df5e341010419b31d0c93
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-18 15:54:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  puts "   |   |   \n"
+  puts "-----------\n"
+  puts "   |   |   \n"
+  puts "-----------\n"
+  puts "   |   |   \n"
+end
+
+display_board
